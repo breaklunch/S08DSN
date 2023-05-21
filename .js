0296b90f714201e3d6a53bccf1bcccb1dc870171
breaklunch <elias.ibanez@tecsup.edu.pe>
2023-05-20 20:50:16 -0500

    first commit

---
+++ .js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+
+const port = 3000; 
+
+
+
+const server = http.createServer((req, res) => {
+
+
+res.statusCode = 200;
+
+
+res.setHeader('Content-Type','text/plain');
+
+
+res.end('Hola a todos les dice su docente');
+
+
+});
+
+
+server.listen(port,() => {
+
+
+console.log('Server corriendo en el puerto 3000');
+
+
+
+});
